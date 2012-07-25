5c605d015cf79ba08ff34dc247ae71dbc2f88909
helibo <1287175@gmail.com>
2012-07-26 01:46:11 +0400

    Done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 App3::Application.routes.draw do
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
